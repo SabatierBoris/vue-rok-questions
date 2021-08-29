b8974dbabe8f3b85ec79527c2adb49130037d6b6
Boris Sabatier <boris@sabatier.io>
2021-08-29 19:42:23 +0200

    UI: Add vue-fuse component and display answers

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,84 @@
           </span>
         </p>
         <p class="control is-expanded has-icons-left">
-          <input class="input" type="text" placeholder="" autofocus>
+          <VueFuse :fuse-opts="options" :list="questions" :defaultAll="true" @fuse-results="handleResults" class="input" autofocus/>
           <span class="icon is-small is-left">
             <i class="fas fa-search"></i>
           </span>
         </p>
         <p class="control">
-          <a class="button is-static">XX questions</a>
+          <a class="button is-static">{{ results.length }} questions</a>
         </p>
       </div>
+      <Question v-for="(question, i) in results" :data="question" :key="i" />
     </div>
   </div>
 </template>
 
 <script>
+import VueFuse from 'vue-fuse'
+import Question from './components/Question.vue'
+
 export default {
   name: 'App',
+  components: {
+    VueFuse,
+    Question,
+  },
+  data () {
+    return {
+      results: [],
+      options: {
+        keys: [
+          {
+            name: 'question',
+            weight: 2,
+          },
+          'good_answers',
+        ],
+        includeScore: true,
+        includeMatches: true,
+        minMatchCharLength: 2,
+      },
+      questions: [{
+        question: "Raphaël est principalement connu pour laquelle des œuvres suivantes ?",
+        good_answers: [
+          "L'École d'Athènes"
+        ],
+        bad_answers: [
+          "La Cène",
+          "Les décorations murales de la Chapelle Sixtine",
+          "Mona Lisa"
+        ],
+      }, {
+        question: "Quel commandant était considéré comme un ennemi de Rome depuis ses jeunes jours ?",
+        good_answers: [
+          "Hannibal Barca"
+        ],
+        bad_answers: [
+          "Särka",
+          "Frédéric 1er",
+          "Boadicée",
+          "Sârka",
+          "Frédéric ler",
+          "Séärka",
+          "Sérka"
+        ],
+      }, {
+        question: "Combien de phases compose l’événement Le plus grand gouverneur ?",
+        good_answers: ["6"],
+        bad_answers: [
+          "7",
+          "4"
+        ],
+      }]
+    };
+  },
+  methods: {
+    handleResults (r) {
+      this.results = r
+    },
+  },
 }
 </script>
 
---
+++ src/components/Question.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tile is-child box">
+    <p class="title">{{ data.item.question }}</p>
+    <p>{{ data.item.good_answers.join(', ') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Question',
+  props: {
+    data: Object
+  }
+}
+</script>
