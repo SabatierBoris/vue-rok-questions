84c0985514faa25bd3d8e5b8f6e12d3e07d4287a
Boris Sabatier <boris@sabatier.io>
2021-08-29 10:00:27 +0200

    UI: Add Bulma and FA

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="tile is-ancestor is-vertical">
+      <div class="tile is-child box field has-addons">
+        <p class="control">
+          <span class="select">
+            <select>
+              <option>Français</option>
+            </select>
+          </span>
+        </p>
+        <p class="control is-expanded has-icons-left">
+          <input class="input" type="text" placeholder="" autofocus>
+          <span class="icon is-small is-left">
+            <i class="fas fa-search"></i>
+          </span>
+        </p>
+        <p class="control">
+          <a class="button is-static">XX questions</a>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "./scss/main.scss";
 </style>
